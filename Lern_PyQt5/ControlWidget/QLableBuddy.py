191074c221e023baefdfbe51fe8549e64f160c64
MmMmMqianq <13813365820@163.com>
2021-09-07 16:11:17 +0800

    GridLayout

--- Lern_PyQt5/ControlWidget/QLableBuddy.py
+++ Lern_PyQt5/ControlWidget/QLableBuddy.py
@@ -1,6 +1,5 @@
 """
-QLabel 与伙伴关系
-
+QLabel 与伙伴关系:
 QLabel对象.setText("显示文本（热键）")
 QLabel对象.setBuddy(控件对象)
 例：
@@ -8,9 +7,13 @@ self.name_label = QLabel(self.widget)
 self.name_label.setText("name(&q)")  # Alt+q
 self.name_line_edit = QLineEdit(self.widget)
 self.name_label.setBuddy(self.name_line_edit
+
+QGridLayout:
+g_layout.addWidget(self.button_cancel, 3, 2, 1, 1)
+g_layout.addWidget(控件对象, 开始行号, 开始列号, 所占行数, 所占列数)
 """
 import sys
-from PyQt5.QtWidgets import QMainWindow, QLabel, QLineEdit, QApplication, QWidget, QGridLayout
+from PyQt5.QtWidgets import QMainWindow, QLabel, QLineEdit, QApplication, QWidget, QGridLayout, QPushButton
 from PyQt5.QtCore import QRect
 
 
@@ -34,11 +37,19 @@ class QlabelBuddy(QMainWindow):
 		self.password_line_dit = QLineEdit(self.widget)
 		self.password_label.setBuddy(self.password_line_dit)
 
+		self.button_ok = QPushButton(self.widget)
+		self.button_ok.setText("Confirm")
+
+		self.button_cancel = QPushButton(self.widget)
+		self.button_cancel.setText("Cancel")
+
 		self.g_layout = QGridLayout(self.widget)
 		self.g_layout.addWidget(self.name_label, 0, 0, 1, 1)
 		self.g_layout.addWidget(self.name_line_edit, 0, 1, 1, 2)
 		self.g_layout.addWidget(self.password_label, 1, 0, 1, 1)
 		self.g_layout.addWidget(self.password_line_dit, 1, 1, 1, 2)
+		self.g_layout.addWidget(self.button_ok, 3, 1, 1, 1)
+		self.g_layout.addWidget(self.button_cancel, 3, 2, 1, 1)
 
 		self.setCentralWidget(self.central_widget)
 
