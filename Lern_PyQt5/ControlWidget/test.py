f79189809b06b1bfc0610b234213ffbea6e851fa
MmMmMqianq <13813365820@163.com>
2021-09-10 18:21:19 +0800

    QLineEditValidator

--- Lern_PyQt5/ControlWidget/test.py
+++ Lern_PyQt5/ControlWidget/test.py
@@ -1,20 +1,13 @@
 import sys
-from PyQt5.QtWidgets import QWidget, QLineEdit ,QApplication, QFormLayout
+from PyQt5.QtWidgets import QWidget, QLineEdit, QApplication, QFormLayout, QAction
 from PyQt5.QtGui import QIntValidator, QDoubleValidator, QRegExpValidator
 
 
 class QLineEditValidator(QWidget):
 	def __init__(self):
+
 		super(QLineEditValidator, self).__init__()
-
-		self.line_edit_int = QLineEdit(self)
-		self.line_edit_Double = QLineEdit(self)
-		self.line_edit_reg = QLineEdit(self)
-
-		self.form = QFormLayout(self)
-		self.form.addRow("QIntValidator", self.line_edit_int)
-		self.form.addRow("QDoubleValidator", self.line_edit_Double)
-		self.form.addRow("QRegValidator", self.line_edit_reg)
+		self.line = QLineEdit(self)
 
 
 if __name__ == "__main__":
