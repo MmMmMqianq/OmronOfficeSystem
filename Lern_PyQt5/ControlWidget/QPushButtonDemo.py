679933146b5897ed4c045000f170cbe9aff47e4a
MmMmMqianq <13813365820@163.com>
2021-09-17 13:41:33 +0800

    QAbstractButton.setShortcut1

--- Lern_PyQt5/ControlWidget/QPushButtonDemo.py
+++ Lern_PyQt5/ControlWidget/QPushButtonDemo.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QWidget, QApplication, QPushButton, QVBoxLayout
+from PyQt5.QtWidgets import QWidget, QApplication, QPushButton, QVBoxLayout, QShortcut
 from PyQt5.QtGui import QIcon, QKeySequence
 from PyQt5.QtCore import QSize, Qt
 
@@ -17,14 +17,31 @@ class QPushButtonDemo(QWidget):
 
 		self.push_button.setIcon(QIcon("./images/1.ico"))
 		self.push_button.setIconSize(QSize(20, 20))
-		print(self.push_button.iconSize())
+		print("1.", self.push_button.iconSize())
 
 		self.push_button.setCheckable(False)  # 如果设置为True按钮按下去不会弹起来的，处于被选中的状态
-		print(self.push_button.isCheckable())
+		print("2.", self.push_button.isCheckable())
 		self.push_button.setChecked(True)  # 将按钮设置为选中状态，只有当isCheckable() = True时改方法才有效
-		print(self.push_button.isChecked())
+		print("3.", self.push_button.isChecked())
 
-		self.push_button.setShortcut(QKeySequence(Qt.CTRL+Qt.Key_Q))
+		self.push_button.setShortcut(QKeySequence(Qt.CTRL+Qt.Key_W))  # mac中ctrl时command键
+		print("4.", self.push_button.shortcut().toString())  # QKeySequence.toString()将快捷键转换为字符串
+
+		self.push_button.clicked.connect(self.clicked_button)  # 按下去松开为clicked
+		self.push_button.pressed.connect(self.pressed_button)  # 按下去为pressed
+		self.push_button.released.connect(self.released_button)  # 松开按钮为released
+
+	def clicked_button(self):
+		print("按钮clicked")
+
+	def pressed_button(self):
+		print("按钮pressed")
+
+	def released_button(self):
+		print("按钮released")
+
+	def toggled_button(self):
+		print("按钮toggled")
 
 
 if __name__ == "__main__":
